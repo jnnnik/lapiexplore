2ed13bb3678feffe7eec0b614ac78994a5dc9dab
Jannik Lemberg <smn@Janniks-MBP.fritz.box>
2015-05-23 13:49:14 +0200

    well i checked in m4l and it still works minified, wohoo!

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> - <%= pkg.author %>, <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: 'src/<%= pkg.name %>.js',
