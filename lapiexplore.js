ea6f35e5f2073fbbf370ef1bb94f82e8393ddb81
Jannik Lemberg <smn@Janniks-MBP.fritz.box>
2015-05-23 13:02:00 +0200

    redid many a thing regarding navigation and such

--- lapiexplore.js
+++ lapiexplore.js
@@ -1,15 +1,9 @@
-var apiNodes = {};
-var OFFSET_DEVICES = 2;
-  
+var apiNodes = {};  
 function getApiNode(path) {
   if(!apiNodes[path]) {
     apiNodes[path] = new ApiNode(path);
   }
   return apiNodes[path];
-}
-
-function isInt(number) {
-  return !isNaN(parseInt(number)) && isFinite(number);
 }
 
 function us2cc(string) {
@@ -21,87 +15,101 @@ function us2cc(string) {
   return parts.join('');
 }
 
-function ApiNode(path) {
+function ApiNode(_path) {
   var self = this;
-  if(!path) path = 'live_set';
-  this.path = path;
-  this.api = new LiveAPI(path);
-  this.parent = null;
-  this.children = null;
+  if(!_path) _path = 'live_set';
+  var _api = new LiveAPI(_path);
+  this.getPath = function() {
+    return _api.unquotedpath;
+  }
   this.getInfo = function() {
-    return self.api.info;
-  }
-  this.getParent = function() {
-    _initParent();
-    return self.parent;
-  }
-  var _initParent = function() {
-    if(self.parent !== null) return;
-    var splitPath = self.path.split(' ');
-    if(splitPath.length === 1) {
-      throw new Error('Can\'t getParent() of root node');
-    }
-    splitPath.pop();
-    self.parent = getApiNode(splitPath.join(' '));
-  }
-  this.getChildren = function() {
-    _initChildren();
-    return self.children;
-  }
-  var _initChildren = function() {
-    if(self.children !== null) return;
-    self.children = [];
-    var apiChildren = self.api.children;
-    if(!isInt(apiChildren[0])) {
-      for(var key in apiChildren) {
-        self.children[self.children.length] =
-          getApiNode(self.path + ' ' + apiChildren[key]);
-      }
-    } else {
-      for(var i=0; i<apiChildren[0]; i++) {
-        self.children[i] =
-          getApiNode(self.path + ' ' + i);
-      }
-    }
+    return _api.info;
   }
   var _addProperty = function(prop) {
     self[us2cc('get_'+prop)] = (function(p){
       return function(){
-        return self.api.get(p);
+        return _api.get(p);
       };
     })(prop);
     self[us2cc('set_'+prop)] = (function(p){
       return function(arg){
-        return self.api.set(p,arg);
+        return _api.set(p,arg);
       };
     })(prop);
   }
   var _addFunction = function(func) {
     self[us2cc(func)] = (function(f){
       return function(arg){
-        return self.api.call(f,arg);
+        return _api.call(f,arg);
       };
     })(func);
   }
-  this.getChildByName = function(name) {
-    var children = this.getChildren();
-    for(var i=0,j=children.length;i<j;i++) {
-      if(children[i].getName && children[i].getName() == name) {
-        return children[i];
-      }
+  var _addChildProperty = function(name, isCollection) {
+    
+    self[us2cc('get_'+name)] = (function(n){
+      return function(){
+        return getApiNode(self.getPath() + ' ' + name);
+      };
+    })(name);
+    if(isCollection) {
+      self[us2cc('get_'+name.substr(0,name.length-1)+'_by_name')] = (function(n) {
+        return function(searchName) {
+          var collection = self[us2cc('get_'+n)]().get();
+          for(var i=0, j=collection.length;i<j; i++) {
+            var node = collection[i];
+            if(node.getName && node.getName() == searchName) {
+              return node;
+            }
+          }
+          return false;
+        };
+      })(name)
     }
   }
   var infoLines = this.getInfo().split("\n");
-  for(var i=0,j=infoLines.length;i<j;i++) {
-    var line = infoLines[i];
-    var name = line.split(' ')[1];
-    if(line.indexOf('property') === 0) {
-      _addProperty(name);
-    } else if(line.indexOf('function') === 0) {
-      _addFunction(name);
+
+  if(infoLines[1] == 'type Vector') {
+    var _childCount = _api.children[0];
+    var _children = [];
+    for(var i=0;i<_childCount;i++) {
+      _children[i] = getApiNode(_path + ' ' + i);
+    }
+    this.get = function(offset) {
+      if(offset === undefined) {
+        return _children;
+      }
+      return _children[offset];
+    }
+    this.getFirst = function() {
+      return _children[0];
+    }
+    this.getLast = function() {
+      return _children[_childCount -1];
+    }
+    this.getCount = function() {
+      return _childCount;
+    }
+  } else {
+    this.getParent = function() {
+      var parentPath = self.getPath() + ' canonical_parent';
+      var check = new LiveAPI(parentPath);
+      if(check.info === '"No object"') {
+        return false;
+      }
+      return getApiNode(parentPath);
+    }
+    for(var i=0,j=infoLines.length;i<j;i++) {
+      var line = infoLines[i];
+      var splitLine = line.split(' ');
+      if(splitLine.length != 3) continue;
+      var name = splitLine[1];
+      if(line.indexOf('property') === 0 && splitLine[2].indexOf('Device') === -1) {
+        _addProperty(name);
+      } else if(line.indexOf('function') === 0) {
+        _addFunction(name);
+      } else if(line.indexOf('child') === 0 || splitLine[2].indexOf('Device') === 0) {
+        _addChildProperty(name, (line.indexOf('children') === 0));
+      }
     }
   }
 }
-
-var n = getApiNode('live_set');
-n.setTempo(120);
